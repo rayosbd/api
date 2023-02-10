a4ee60641cf2d518e11c8df38273d8b25e4a553e
tam11a <ibrahimsadiktamim@gmail.com>
2023-02-10 22:31:54 +0600

    updated: admin login issue

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -2,81 +2,81 @@ const Admin = require("../../model/Admin");
 const ErrorResponse = require("../../utils/errorResponse");
 
 exports.register = async (req, res, next) => {
-  // Get Values
-  const { userName, phone, avatarUrl, password } = req.body;
+	// Get Values
+	const { userName, phone, avatarUrl, password } = req.body;
 
-  try {
-    // Store Admin to DB
-    const user = await Admin.create({
-      userName,
-      phone,
-      avatarUrl,
-      password,
-    });
+	try {
+		// Store Admin to DB
+		const user = await Admin.create({
+			userName,
+			phone,
+			avatarUrl,
+			password,
+		});
 
-    // Send Success Response & Login Token
-    sendToken(user, 201, res);
+		// Send Success Response & Login Token
+		sendToken(user, 201, res);
 
-    // On Error
-  } catch (error) {
-    // Send Error Response
-    next(error);
-  }
+		// On Error
+	} catch (error) {
+		// Send Error Response
+		next(error);
+	}
 };
 
 exports.login = async (req, res, next) => {
-  const { phone, password } = req.body;
+	const { phone, password } = req.body;
 
-  if (!phone || !password)
-    return next(ErrorResponse("Please provide phone and password", 400));
+	if (!phone || !password)
+		return next(ErrorResponse("Please provide phone and password", 400));
 
-  try {
-    const user = await Admin.findOne({
-      phone,
-      isVerified: true,
-      isActive: true,
-    }).select("+password");
+	try {
+		const user = await Admin.findOne({
+			phone,
+			isVerified: true,
+			isActive: true,
+		}).select("+password");
 
-    // Send Error if No Admin Found
-    if (!user) next(new ErrorResponse("Invalid credentials", 401));
+		// Send Error if No Admin Found
+		if (!user) next(new ErrorResponse("Invalid credentials", 401));
 
-    // Check if the password is corrent
-    const isMatch = await user.matchPasswords(password);
-    if (!isMatch) next(new ErrorResponse("Incorrect password", 401));
+		// Check if the password is corrent
+		const isMatch = await user.matchPasswords(password);
+		if (!isMatch) return next(new ErrorResponse("Incorrect password", 401));
 
-    // Send Success Response
-    sendToken(user, 200, res);
-    // On Error
-  } catch (error) {
-    // Send Error Response
-    next(error);
-  }
+		// Send Success Response
+		sendToken(user, 200, res);
+		// On Error
+	} catch (error) {
+		// Send Error Response
+		next(error);
+	}
 };
 
 exports.forgetpassword = async (req, res, next) => {
-  res.send("Forget Password Route");
+	res.send("Forget Password Route");
 };
 
 exports.resetpassword = async (req, res, next) => {
-  res.send("Reset Password Route");
+	res.send("Reset Password Route");
 };
 
 exports.validate = async (req, res, next) => {
-  if (req.user)
-    res.json({
-      success: true,
-      data: req.user,
-    });
-  else {
-    return next(ErrorResponse("No Admin Found!", 404));
-  }
+	if (req.user)
+		res.json({
+			success: true,
+			data: req.user,
+		});
+	else {
+		return next(ErrorResponse("No Admin Found!", 404));
+	}
 };
 
 const sendToken = (user, statusCode, res) => {
-  res.status(statusCode).json({
-    success: true,
-    token: user.getSignedToken(), // generates token
-  });
+	res.status(statusCode).json({
+		success: true,
+		token: user.getSignedToken(), // generates token
+	});
 };
 
 // https://youtu.be/YocRq-KesCM
